ad75a2c4fbba47355953ca0b8fb772ceb2de0e68
Ednario Andrade <joseednario637@gmail.com>
2022-12-30 00:38:07 -0300

    feat: ensure SignUpControlller returns 400 if password confirmation fails

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -22,6 +22,10 @@ export class SignUpController implements Controller {
         }
       }
 
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+
       const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
       if (!emailIsValid) {
         return badRequest(new InvalidParamError('email'))
